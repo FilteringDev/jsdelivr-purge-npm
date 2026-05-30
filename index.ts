152c790ae83a48f5959516d35d88632e02fb151e
PiQuark6046 <piquark6046@proton.me>
2026-05-30 13:03:40 +0000

    refactor: streamline option parsing and update dependencies

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import * as Commander from 'commander'
 import * as Actions from '@actions/core'
 import * as Os from 'node:os'
 import * as Fs from 'node:fs'
 import * as Path from 'node:path'
+import { FilterArgumentsForOptions, ParseArgumentsAndOptions } from '@typescriptprime/parsing'
 import { Piscina } from 'piscina'
 import { fileURLToPath } from 'node:url'
 import { RequestNpmPackageMetaData } from './sources/npm-api.js'
@@ -15,55 +15,37 @@ export type TagWorkerDataType = {
 	OlderTags: IHistoryManagerDataJSON | null
 }
 
+type OptionsType = {
+	GitHubToken: string
+	Package: string
+	CIWorkspacePath: string
+	CIActionPath: string
+	WorkflowRef: string
+	DistTag: string
+	Repo: string
+}
+
 const CurrentFilename = fileURLToPath(import.meta.url)
 const CurrentDirname = Path.dirname(CurrentFilename)
 const TagWorkerFilename = Path.join(CurrentDirname, 'sources', 'tag-worker.ts')
 
 Actions.info(`Running on ${Os.cpus()[0].model} with ${Os.cpus().length} threads/vCPUs.`)
 
-const Program = new Commander.Command()
+const Options = await ParseOptions(process.argv)
 
-// Set options.
-Program.option('--gh-token <TOKEN>', 'GitHub token', '')
-	.option('--package <package>', 'A npm package. eg: owner/repo', '')
-	.option('--ci-workspace-path <PATH>', 'A path to the CI workspace.', '')
-	.option('--ci-action-path <PATH>', 'A path to the CI action.', '')
-	.option('--workflow-ref <WORKFLOW_REF>', 'A GitHub workflow ref. eg: refs/heads/master', '')
-	.option('--dist-tag <DIST_TAG>', 'A npm dist-tag. eg: latest', '')
-	.option('--repo <REPO>', 'A GitHub repository. eg: owner/repo', '')
-
-// Initialize Input of the options and export them.
-Program.parse()
-
-const Options = Program.opts() as {
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	ghToken: string
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	package: string
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	ciWorkspacePath: string
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	ciActionPath: string,
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	workflowRef: string
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	distTag: string
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	repo: string
-}
-
-const CurrrentTags = (await RequestNpmPackageMetaData(Options.package))['dist-tags']
+const CurrrentTags = (await RequestNpmPackageMetaData(Options.Package))['dist-tags']
 const DistTagFilePath = CreateDistTagFilePath()
 Fs.writeFileSync(DistTagFilePath, JSON.stringify(CurrrentTags), { mode: 0o600 })
-const OlderTags = await new HistoryManager({ Repo: Options.repo, GitHubToken: Options.ghToken, WorkflowRef: Options.workflowRef }).RequestHistory()
+const OlderTags = await new HistoryManager({ Repo: Options.Repo, GitHubToken: Options.GitHubToken, WorkflowRef: Options.WorkflowRef }).RequestHistory()
 const PiscinaInstance = new Piscina({
 	filename: TagWorkerFilename,
-	maxThreads: Os.cpus().length
+	maxThreads: Os.cpus().length,
+	execArgv: [...process.execArgv]
 })
 
 try {
-	await Promise.all(Options.distTag.split(' ').filter(TargetTag => TargetTag.length > 0).map(TargetTag => PiscinaInstance.run({
-		Package: Options.package,
+	await Promise.all(Options.DistTag.split(' ').filter(TargetTag => TargetTag.length > 0).map(TargetTag => PiscinaInstance.run({
+		Package: Options.Package,
 		TargetTag,
 		CurrentTags: CurrrentTags,
 		OlderTags
@@ -81,4 +63,30 @@ function CreateDistTagFilePath(): string {
 	}
 
 	return DistTagFilePath
+}
+
+async function ParseOptions(Argv: string[]): Promise<OptionsType> {
+	const { Options } = await ParseArgumentsAndOptions<Record<string, string | boolean>>(FilterArgumentsForOptions(Argv), {
+		NamingConvention: FormatOptionName
+	})
+
+	return {
+		GitHubToken: GetOptionValue(Options, 'GitHubToken'),
+		Package: GetOptionValue(Options, 'Package'),
+		CIWorkspacePath: GetOptionValue(Options, 'CIWorkspacePath'),
+		CIActionPath: GetOptionValue(Options, 'CIActionPath'),
+		WorkflowRef: GetOptionValue(Options, 'WorkflowRef'),
+		DistTag: GetOptionValue(Options, 'DistTag'),
+		Repo: GetOptionValue(Options, 'Repo')
+	}
+}
+
+function FormatOptionName(OptionName: string): string {
+	return OptionName.replace(/^--/, '').replaceAll(/-([a-z])/g, (FullMatch: string, Character: string) => Character.toUpperCase())
+}
+
+function GetOptionValue(Options: Record<string, string | boolean>, OptionName: string): string {
+	const Value = Options[OptionName]
+
+	return typeof Value === 'string' ? Value : ''
 }
